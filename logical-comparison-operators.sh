0dedd7a1501f9d16073c64a34d84c7afbadd3945
Laura <blakami@autonomia.digital>
2024-08-02 16:38:53 -0500

    Logical comparison operators scripts

--- logical-comparison-operators.sh
+++ logical-comparison-operators.sh
@@ -4,4 +4,15 @@ if [[ -e "$1" && -r "$1" ]]
 then
     echo -e "We can read the file that has been specified"
     exit 0
-elif [[  ]]
+elif [[ ! -e "$1" ]]
+then
+    echo -e "The specified file does not exist."
+    exit 2
+elif [[ -e "$1" && ! -r "$1" ]]
+then
+    echo -e "We don't have read permission for this file"
+    exit 1
+else
+    echo -e "Error ocurred"
+    exit 5
+fi
